b46b0f0886e112d25c199de80beb9ee5a84534c8
KFerrier <kristinferrier@gmail.com>
2018-07-06 12:41:50 -0500

    Add Expensify styles

--- src/app.js
+++ src/app.js
@@ -10,6 +10,7 @@ import "normalize.css/normalize.css";
 import "./styles/styles.scss";
 import "react-dates/lib/css/_datepicker.css";
 import { firebase } from "./firebase/firebase";
+import LoadingPage from "./components/LoadingPage";
 
 const store = configureStore();
 
@@ -28,12 +29,12 @@ const renderApp = () => {
     }
 };
 
-ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
+ReactDOM.render(<LoadingPage />, document.getElementById('app'));
 
 firebase.auth().onAuthStateChanged((user) => {
     if (user) {
-        console.log("login");
-        console.log("uid: ", user.uid);
+        //console.log("login");
+        //console.log("uid: ", user.uid);
         store.dispatch(login(user.uid));
         store.dispatch(startSetExpenses()).then(() => {
             renderApp();
@@ -43,7 +44,7 @@ firebase.auth().onAuthStateChanged((user) => {
             }
         });
     } else {
-        console.log("log out");
+        //console.log("log out");
         store.dispatch(logout());
         renderApp();
         //send to application root (login page)
